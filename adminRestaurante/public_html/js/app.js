2b066df3409f633e47843b5df4c773fb53761f96
Jose Carlos Jimenez Montenegro <josekrlos029@hotmail.com>
2014-07-04 19:49:14 -0500

    Primera Prueba !

--- adminRestaurante/public_html/js/app.js
+++ adminRestaurante/public_html/js/app.js
@@ -28,6 +28,47 @@ function update() {
 
 }
 
-function cargarPedidios(){
-    
+function cargarPedidios() {
+
+    var $this = $(this),
+            theme = $this.jqmData("theme") || $.mobile.loader.prototype.options.theme,
+            msgText = $this.jqmData("msgtext") || $.mobile.loader.prototype.options.text,
+            textVisible = $this.jqmData("textvisible") || $.mobile.loader.prototype.options.textVisible,
+            textonly = !!$this.jqmData("textonly");
+    html = $this.jqmData("html") || "";
+    $.mobile.loading("show", {
+        text: msgText,
+        textVisible: textVisible,
+        theme: theme,
+        textonly: textonly,
+        html: html
+    });
+
+    var idRestaurante = localStorage.getItem("idRestaurante");
+
+    var data = {
+        idRestaurante: idRestaurante
+    };
+    var url = "http://tudomicilio.liceogalois.com/restaurante/domicilios";
+    //var url = "http://192.168.1.33/domicilios/restaurante/domicilios";
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: data
+    }).done(function(msg) {
+
+        $("#tabs").append(msg);
+        setTimeout(function() {
+            $('#lista1').trigger('create');
+            $('#lista2').trigger('create');
+            $('#lista3').trigger('create');
+            $(".l1").trigger('create');
+            $(".l2").trigger('create');
+            $.mobile.loading("hide");
+        }, 2000);
+
+
+
+    });
+
 }
